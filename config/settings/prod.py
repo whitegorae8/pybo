4c1d4f2d2019da0b2b101bc675447afe35af95c6
whitegorae8 <whitegorae8@gmail.com>
2023-07-25 11:12:04 +0900

    데이터베이스 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['54.180.64.207', 'assist-8.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'iY0>(J]V:KAoWv0hp$9h^yzMuFp)^8ty',
+        'HOST': 'ls-b85d0f54303b34b782d62b0a219fc938d131bf81.cpqu19dtplyc.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
